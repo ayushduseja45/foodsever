3b2508ad47464703901ab4fe3c891e896859e478
ayushduseja45 <dusejaayush2910@gmail.com>
2024-03-08 00:47:47 +0530

    project done in one commit

--- js/config.js
+++ js/config.js
@@ -1,12 +1,22 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyB7USYZHtyW6YbXVGH_cBUWPphP4QjW01o",
-    authDomain: "west-food-management.firebaseapp.com",
-    databaseURL: "https://west-food-management-default-rtdb.firebaseio.com",
-    projectId: "west-food-management",
-    storageBucket: "west-food-management.appspot.com",
-    messagingSenderId: "982475828562",
-    appId: "1:982475828562:web:d7ef90ca8a58b284bc5e8b",
-    measurementId: "G-8SRC0DS1F7"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB7USYZHtyW6YbXVGH_cBUWPphP4QjW01o",
+//     authDomain: "west-food-management.firebaseapp.com",
+//     databaseURL: "https://west-food-management-default-rtdb.firebaseio.com",
+//     projectId: "west-food-management",
+//     storageBucket: "west-food-management.appspot.com",
+//     messagingSenderId: "982475828562",
+//     appId: "1:982475828562:web:d7ef90ca8a58b284bc5e8b",
+//     measurementId: "G-8SRC0DS1F7"
+// };
+const firebaseConfig = {
+    apiKey: "AIzaSyAagBwGx8rTWXTzyAyL5d1bcrVEarbqlE0",
+    authDomain: "foodsaver-c3df4.firebaseapp.com",
+    databaseURL: "https://foodsaver-c3df4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "foodsaver-c3df4",
+    storageBucket: "foodsaver-c3df4.appspot.com",
+    messagingSenderId: "1003831307855",
+    appId: "1:1003831307855:web:5e8d885649d8fa2c77c43a",
+    measurementId: "G-QCQ0LXK0F5"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
